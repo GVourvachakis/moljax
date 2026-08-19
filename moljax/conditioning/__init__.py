c250dc5b1453e81e7f70f8cbff8d67312fbdcf0f
G_V <vourvachakisgeorgios@gmail.com>
2026-08-19 17:06:48 +0300

    conditioning: add forward-only Arnoldi pseudospectra diagnostics

--- moljax/conditioning/__init__.py
+++ moljax/conditioning/__init__.py
@@ -1,5 +1,22 @@
 """Matrix-free conditioning diagnostics."""
 
 from .field_of_values import FieldOfValuesResult, numerical_range
+from .pseudospectra import (
+    PseudospectraResult,
+    arnoldi,
+    epsilon_zero,
+    pseudospectrum_dense,
+    reduced_pseudospectrum,
+    ritz_values,
+)
 
-__all__ = ["FieldOfValuesResult", "numerical_range"]
+__all__ = [
+    "FieldOfValuesResult",
+    "PseudospectraResult",
+    "arnoldi",
+    "epsilon_zero",
+    "numerical_range",
+    "pseudospectrum_dense",
+    "reduced_pseudospectrum",
+    "ritz_values",
+]
