f3b41bcefff87c176ec2cf2d02897e011a40ac27
G_V <vourvachakisgeorgios@gmail.com>
2026-08-19 17:37:12 +0300

    conditioning: add non-normality rate estimates and preconditioner decision procedure

--- moljax/conditioning/__init__.py
+++ moljax/conditioning/__init__.py
@@ -1,6 +1,17 @@
 """Matrix-free conditioning diagnostics."""
 
 from .field_of_values import FieldOfValuesResult, numerical_range
+from .non_normality import (
+    PreconditionerAssessment,
+    RateEstimates,
+    assess_preconditioner,
+    clustering_rate,
+    crouzeix_palencia_envelope,
+    enclosing_disk_rate,
+    estimate_rates,
+    right_real_outliers,
+    traced_boundary_rate,
+)
 from .pseudospectra import (
     PseudospectraResult,
     arnoldi,
@@ -13,10 +24,19 @@ from .pseudospectra import (
 __all__ = [
     "FieldOfValuesResult",
     "PseudospectraResult",
+    "PreconditionerAssessment",
+    "RateEstimates",
     "arnoldi",
+    "assess_preconditioner",
+    "clustering_rate",
+    "crouzeix_palencia_envelope",
     "epsilon_zero",
+    "enclosing_disk_rate",
+    "estimate_rates",
     "numerical_range",
     "pseudospectrum_dense",
     "reduced_pseudospectrum",
+    "right_real_outliers",
     "ritz_values",
+    "traced_boundary_rate",
 ]
